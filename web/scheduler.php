d168cc31a95073e996b5eb7a6282a8efc154c8a6
Sergey Kruzhkov <sergey@dyrex.net>
2020-06-09 14:39:00 +0300

    Update scheduler.php

--- web/scheduler.php
+++ web/scheduler.php
@@ -10,17 +10,6 @@
 
 		if ($seconds=="00") :	
 			$current_time = date("H:i");
-
-			$current_duration = (int)($s->off_tod);
-
-			$current_end = '';
-			
-			if ((string)$s->on_tod != '') :
-				$d = new DateTime('2000-01-01 ' . $s->on_tod . ':00');
-				$interval = DateInterval::createFromDateString((string)$current_duration . " min");
-				$d->add($interval);
-				$current_end = $d->format('H:i');
-			endif;
 	
 			$current_dow = date("N"); 
 		
@@ -33,6 +22,17 @@
 			foreach ($sched as $s) :
 		
 				if ($s->enabled):
+
+					$current_duration = (int)($s->off_tod);
+
+					$current_end = '';
+
+					if ((string)$s->on_tod != '') :
+						$d = new DateTime('2000-01-01 ' . $s->on_tod . ':00');
+						$interval = DateInterval::createFromDateString((string)$current_duration . " min");
+						$d->add($interval);
+						$current_end = $d->format('H:i');
+					endif;
 				
 					if (strpos($s->on_dow,  $current_dow)!== false) :
 						if ( $s->on_tod==$current_time  ) call_HA($s->entity_id,"on");
