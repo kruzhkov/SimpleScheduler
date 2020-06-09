619c63a3d17eb1b943c489e1c594ea28622a3809
Sergey Kruzhkov <sergey@dyrex.net>
2020-06-09 12:15:39 +0300

    Update scheduler.php

--- web/scheduler.php
+++ web/scheduler.php
@@ -33,7 +33,7 @@ echo $current_duration;
 
 			$current_end = '';
 			$d = new DateTime($current_time);
-// 			$interval = DateInterval::createFromDateString((string)$current_duration + " min");
+			$interval = DateInterval::createFromDateString((string)$current_duration + " min");
 // 			$d->add($interval);
 // 			$current_end = (string)$d;
 	
